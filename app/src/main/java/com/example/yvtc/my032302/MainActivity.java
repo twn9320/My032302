186b0c9b9c65d4289f696b0b7006d4236d5e8787
twn9320 <twn9320@gmail.com>
2017-03-23 09:18:58 +0800

    三顆按鍵

--- app/src/main/java/com/example/yvtc/my032302/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my032302/MainActivity.java
@@ -15,19 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void click1(View v){
-        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this); //設定框架
         builder.setTitle("對話框測試");
         builder.setMessage("測試");
-        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {  //設定按下確認鍵
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("DLG","使用者按下確定");
             }
         });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {  //設定按下取消鍵
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("DLG","使用者按下取消");
+            }
+        });
+        builder.setNeutralButton("看說明", new DialogInterface.OnClickListener() { //第三顆按鍵
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG","使用者按下看說明");
             }
         });
         builder.show();
