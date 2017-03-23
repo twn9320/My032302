27c38826c416db8a42dfb0579bff5c1f5217f57e
twn9320 <twn9320@gmail.com>
2017-03-23 13:36:31 +0800

    自訂(按鍵)

--- app/src/main/java/com/example/yvtc/my032302/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my032302/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import static com.example.yvtc.my032302.R.id.button;
 
 public class MainActivity extends AppCompatActivity {
     int s= -1;
@@ -162,6 +165,14 @@ public class MainActivity extends AppCompatActivity {
 
         LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
         View mv= inflater.inflate(R.layout.custom_dialog_layout,null);
+        Button btn = (Button) mv.findViewById(R.id.button6);
+        final TextView tv5 = (TextView) mv.findViewById(R.id.textView5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv5.setText("Hello World");
+            }
+        });
         builder.setView(mv);
 
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
